e34cc83eff5e0b957c01ed6f3dd202437e61c9b5
Taylor Santos <taylor.p.santos@gmail.com>
2022-06-08 12:09:43 -0700

    Add OpenCL support

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,303 @@
-//
-// Created by taylor-santos on 4/16/2022 at 22:31.
-//
+#define CL_HPP_ENABLE_EXCEPTIONS
+#define CL_HPP_TARGET_OPENCL_VERSION 200
+
+#define NOMINMAX
+#include <GL/gl3w.h>
+#include <CL/opencl.hpp>
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#ifdef WIN32
+#elif __APPLE__
+#else
+#    include <GL/glx.h>
+#endif
+
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wunknown-warning-option"
+#    pragma clang diagnostic ignored "-Wdeprecated-volatile"
+#elif defined(__GNUC__) || defined(__GNUG__)
+#    pragma GCC diagnostic push
+#    pragma GCC diagnostic ignored "-Wpragmas"
+#    pragma GCC diagnostic ignored "-Wvolatile"
+#endif
+
+#define GLM_FORCE_SILENT_WARNINGS
+#include <glm/glm.hpp>
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-#include "glpt.hpp"
+GLuint                  texture;
+cl::ImageGL             cl_texture;
+cl::Context             context;
+cl::Kernel              kernel;
+cl_int                  error = 0;
+std::vector<cl::Memory> img;
+int                     WIDTH = 256, HEIGHT = 256;
+
+static void
+glfw_error_callback(int err, const char *description) {
+    fprintf(stderr, "Glfw Error %d: %s\n", err, description);
+}
+
+static void
+glfw_framebuffer_size_callback(GLFWwindow *, int width, int height) {
+    std::cout << "window resized to " << width << " " << height << "\n";
+    WIDTH  = width;
+    HEIGHT = height;
+    glViewport(0, 0, width, height);
+    glDeleteTextures(1, &texture);
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(
+        GL_TEXTURE_2D,    // target
+        0,                // mipmap level
+        GL_RGBA8,         // internal format
+        width,            // width in pixels
+        height,           // height in pixels
+        0,                // border, must be 0
+        GL_RGBA,          // format
+        GL_UNSIGNED_BYTE, // type
+        nullptr           // data
+    );
+    cl_texture = cl::ImageGL(context, CL_MEM_READ_WRITE, GL_TEXTURE_2D, 0, texture, &error);
+    img        = std::vector<cl::Memory>{cl_texture};
+    kernel.setArg(0, cl_texture);
+}
 
 int
-main(int, char **) {
-    std::cout << glpt::foo() << std::endl;
+main() {
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) return 1;
+
+    // Create window with graphics context
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "GLFW Window", nullptr, nullptr);
+    if (window == nullptr) {
+        return 1;
+    }
+    glfwGetFramebufferSize(window, &WIDTH, &HEIGHT);
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
+    glfwSwapInterval(1); // Enable vsync
+
+    if (gl3wInit()) {
+        fprintf(stderr, "failed to initialize OpenGL\n");
+        return -1;
+    }
+    if (!gl3wIsSupported(3, 2)) {
+        fprintf(stderr, "OpenGL 3.2 not supported\n");
+        return -1;
+    }
+
+    GLuint program = glCreateProgram();
+    {
+        const char *src    = "#version 330\n"
+                             "layout(location = 0) in vec4 vposition;\n"
+                             "layout(location = 1) in vec2 vtexcoord;\n"
+                             "out vec2 ftexcoord;\n"
+                             "void main() {\n"
+                             "    ftexcoord = vtexcoord;\n"
+                             "    gl_Position = vposition;\n"
+                             "}\n";
+        GLuint      shader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(shader, 1, &src, nullptr);
+        glCompileShader(shader);
+        glAttachShader(program, shader);
+    }
+    {
+        const char *src    = "#version 330\n"
+                             "uniform sampler2D tex;\n"
+                             "in vec2 ftexcoord;\n"
+                             "layout(location = 0) out vec4 fcolor;\n"
+                             "void main() {\n"
+                             "    fcolor = texture(tex, ftexcoord);\n"
+                             "}\n";
+        GLuint      shader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(shader, 1, &src, nullptr);
+        glCompileShader(shader);
+        glAttachShader(program, shader);
+    }
+    glLinkProgram(program);
+    glUseProgram(program);
+    GLint tex_loc = glGetUniformLocation(program, "tex");
+
+    GLfloat vertexData[] = {
+        // {x,y,z,u,v}
+        1.0f,  1.0f,  0.0f, 1.0f, 1.0f, // vertex 0
+        -1.0f, 1.0f,  0.0f, 0.0f, 1.0f, // vertex 1
+        1.0f,  -1.0f, 0.0f, 1.0f, 0.0f, // vertex 2
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // vertex 3
+    };
+    GLuint indexData[] = {
+        0,
+        1,
+        2, // triangle 0
+        2,
+        1,
+        3 // triangle 1
+    };
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(
+        1,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        5 * sizeof(GLfloat),
+        (char *)(0 + 3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexData), indexData, GL_STATIC_DRAW);
+
+    glGenTextures(1, &texture);
+
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(
+        GL_TEXTURE_2D,    // target
+        0,                // mipmap level
+        GL_RGBA8,         // internal format
+        WIDTH,            // width in pixels
+        HEIGHT,           // height in pixels
+        0,                // border, must be 0
+        GL_RGBA,          // format
+        GL_UNSIGNED_BYTE, // type
+        nullptr           // data
+    );
+
+    glFinish();
+
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    auto platform = cl::Platform::getDefault();
+    auto device   = cl::Device::getDefault();
+
+#ifdef WIN32
+    cl_context_properties props[] = {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)platform(),
+        CL_GL_CONTEXT_KHR,
+        (cl_context_properties)wglGetCurrentContext(),
+        CL_WGL_HDC_KHR,
+        (cl_context_properties)wglGetCurrentDC(),
+        0};
+#elif __APPLE__
+    CGLContextObj    glContext  = CGLGetCurrentContext();
+    CGLShareGroupObj shareGroup = CGLGetShareGroup(glContext);
+
+    cl_context_properties props[] = {
+        CL_CONTEXT_PROPERTY_USE_CGL_SHAREGROUP_APPLE,
+        (cl_context_properties)shareGroup,
+        0};
+#else
+    cl_context_properties props[] = {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)platform(),
+        CL_GL_CONTEXT_KHR,
+        (cl_context_properties)glXGetCurrentContext(),
+        CL_GLX_DISPLAY_KHR,
+        (cl_context_properties)glXGetCurrentDisplay(),
+        0};
+#endif
+
+    context = cl::Context(device, props);
+    std::string kernel_code =
+        "void kernel my_kernel(__write_only image2d_t output, __read_only uint frame){\n"
+        "    int2 coord = (int2)(get_global_id(0), get_global_id(1));\n"
+        "    int i = coord.x + coord.y * get_image_width(output);\n"
+        "    float3 color = (float3){\n"
+        "        i % 256 / 255.0,\n"
+        "        (i / 256) % 256 / 255.0,\n"
+        "        (i / 256 / 256) % 256 / 255.0\n"
+        "    };\n"
+        "    write_imagef(output, coord, (float4){color, 1.0});\n"
+        "}";
+    auto sources    = cl::Program::Sources{kernel_code};
+    auto cl_program = cl::Program(context, sources);
+    auto queue      = cl::CommandQueue(context, device);
+
+    try {
+        cl_program.build(device);
+    } catch (cl::BuildError &e) {
+        std::cout << "Error building:\n";
+        for (auto &line : e.getBuildLog()) {
+            std::cout << "\t" << line.first.getInfo<CL_DEVICE_NAME>() << ": " << line.second
+                      << "\n";
+        }
+
+        exit(EXIT_FAILURE);
+    }
+
+    try {
+        cl_texture = cl::ImageGL(context, CL_MEM_READ_WRITE, GL_TEXTURE_2D, 0, texture, nullptr);
+    } catch (cl::Error &e) {
+        // TODO: more than just an error code
+        // force this exception by removing `props` from the context constructor
+        std::cout << e.what() << "\n";
+        exit(EXIT_FAILURE);
+    }
+
+    kernel = cl::Kernel(cl_program, "my_kernel");
+    kernel.setArg(0, cl_texture);
+    img = std::vector<cl::Memory>{cl_texture};
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    glm::vec4 clear_color(0.45f, 0.55f, 0.60f, 1.00f);
+    cl_uint   frame_count = 0;
+    auto      prev_time   = glfwGetTime();
+    while (!glfwWindowShouldClose(window)) {
+        frame_count++;
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(program);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(tex_loc, 0);
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        // glFinish();
+
+        glfwSwapBuffers(window);
+
+        kernel.setArg(1, frame_count);
+        queue.enqueueAcquireGLObjects(&img);
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(WIDTH, HEIGHT));
+        queue.finish();
+        queue.enqueueReleaseGLObjects(&img);
+
+        auto curr_time = glfwGetTime();
+        if (curr_time - prev_time > 1.0) {
+            std::cout << frame_count / (curr_time - prev_time) << "\t" << error << std::endl;
+            frame_count = 0;
+            prev_time   = curr_time;
+        }
+    }
+
+    return 0;
 }
